664ee3cd7fac0b32946e9c735b73d4ed39716839
Dev Kansara <83707825+devkansara1310@users.noreply.github.com>
2021-05-09 11:50:43 +0530

    Create Colored_text.py

---
+++ Colored_text.py
@@ -0,0 +1,8 @@
+#Print Colored text
+
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True)
+
+print(Fore.BLUE+Back.YELLOW+"Hii DevOps"+Fore.YEllOW+Back.BLUE+"How are you all !")
+print(Back.CYAN+"I am Dev Kansara")
